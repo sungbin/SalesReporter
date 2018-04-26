a0336a9e2932b8d821184fd6d9fb9a1a92f03f8d
JC <JC@172.17.198.51>
2018-04-26 12:19:28 +0900

    Add Salesman

---
+++ src/main/java/edu/handong/csee/java/example/Salesman.java
@@ -0,0 +1,22 @@
+package edu.handong.csee.java.example;
+
+public class Salesman {
+	private String name;
+	private double sales;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getSales() {
+		return sales;
+	}
+	
+	public void setSales(double sales) {
+		this.sales = sales;
+	}
+}
